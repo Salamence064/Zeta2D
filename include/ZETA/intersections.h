280cb7c11c0fb9412559416be64a31af61b4631b
Salamence064 <thomas.ducote64@gmail.com>
2023-06-09 18:27:53 -0500

    Point v Line

--- include/ZETA/intersections.h
+++ include/ZETA/intersections.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #include "bodies.h"
+
+namespace Collisions {
+    // * ===================================
+    // * Point vs Primitives
+    // * ===================================
+
+    // Determine if a point lays on a line.
+    bool PointAndLine(ZMath::Vec2D const &point, Primitives::Line2D const &line) {
+        // ? Use the point slope form equation of a line to find if the point lies on the line.
+
+        ZMath::Vec2D min = line.getMin(), max = line.getMax();
+        return point.x <= max.x && point.y <= max.y && point.x >= min.x && point.y >= min.y && 
+                point.y == ((line.end.y - line.start.y)/(line.end.x - line.start.x) * (point.x - line.start.x) + line.start.y);
+    };
+}
